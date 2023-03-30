3e760d4b640f29b68792747ef2c06969cbec1634
Gergely Santa <gergely.santa@bitrise.io>
2023-03-30 13:46:24 +0200

    Remove unused files

--- _scripts/webpack_next_js_loader_script.js
+++
@@ -1,9 +0,0 @@
-var axios = require("axios");
-
-axios.get("http://localhost:3387/yml")
-.then((response) => {
-  const htmlContent = response.data.match(/<body[^>]*>((.|[\n\r])*)<script id="__NEXT_DATA__"/);
-
-  console.log(htmlContent[1]);
-})
-.catch((err) => console.log(err));  
